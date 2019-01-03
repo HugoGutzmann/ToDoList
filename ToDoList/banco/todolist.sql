078b0133e393fd445c3dfaae8ccbf64d89cd4083
HugoGutzmann <facahugo@gmail.com>
2019-01-03 18:44:57 -0200

    Add files via upload

--- ToDoList/banco/todolist.sql
+++ ToDoList/banco/todolist.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 02-Jan-2019 às 21:25
+-- Generation Time: 03-Jan-2019 às 21:43
 -- Versão do servidor: 10.1.37-MariaDB
 -- versão do PHP: 7.3.0
 
@@ -36,6 +36,33 @@ CREATE TABLE `afazeres` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
 --
+-- Extraindo dados da tabela `afazeres`
+--
+
+INSERT INTO `afazeres` (`id`, `nome`, `data`, `descricao`) VALUES
+(7, 'hugo', '2019-01-03', 'aaaa');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `finalizadas`
+--
+
+CREATE TABLE `finalizadas` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(100) COLLATE latin1_general_ci NOT NULL,
+  `data` date NOT NULL,
+  `descricao` varchar(800) COLLATE latin1_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+
+--
+-- Extraindo dados da tabela `finalizadas`
+--
+
+INSERT INTO `finalizadas` (`id`, `nome`, `data`, `descricao`) VALUES
+(5, 'terminar hjoje', '2019-01-03', 'asdasd');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -46,6 +73,12 @@ ALTER TABLE `afazeres`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `finalizadas`
+--
+ALTER TABLE `finalizadas`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -53,7 +86,13 @@ ALTER TABLE `afazeres`
 -- AUTO_INCREMENT for table `afazeres`
 --
 ALTER TABLE `afazeres`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `finalizadas`
+--
+ALTER TABLE `finalizadas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
---
+++ ToDoList/index.php
@@ -0,0 +1,3 @@
+<?php
+header("Location: app/controlers/ControlerGeral.php");
+?>
